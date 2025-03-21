2c8c5eb94d27ce1fccbf9e596349fda808764344
Oni Abdullahi Oluwabusayo <busayaooni@gmail.com>
2025-03-21 16:22:56 +0100

    refactor: remove unnecessary scrollTo effect from WalletMain component

--- src/components/WalletMain.tsx
+++ src/components/WalletMain.tsx
@@ -3,10 +3,7 @@ import Payment from '../components/WalletSections/Payment'
 import Transactions from './WalletSections/Transactions'
 
 const WalletMain = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-  
+    
   return (
     <section className='p-5 bg-[#F7F6FB]'>
     <Payment />
