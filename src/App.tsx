7f94cea114286765eff5eb8c380676cc2a6b7d08
Oni Abdullahi Oluwabusayo <busayaooni@gmail.com>
2025-03-21 17:02:54 +0100

    refactor: improve readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import '@mantine/core/styles.css';
 import '@mantine/charts/styles.css';
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 
 import { MantineProvider } from '@mantine/core';
 import LandingPage from './components/LandingPage';
 import WalletManagement from './components/pages/WalletManagement'
 
 export default function App() {
-  return <MantineProvider>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/wallet" element={<WalletManagement />} />
-    </Routes>
-  </BrowserRouter>
-    
-  </MantineProvider>;
+  return (
+    <MantineProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/wallet" element={<WalletManagement />} />
+        </Routes>
+      </Router>
+    </MantineProvider>
+  );
 }
